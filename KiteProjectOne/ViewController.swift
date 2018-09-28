84bfba0c8bf9ea5857202127e95c25a38ed936f5
Zoe Xanos <zxanos@gmail.com>
2018-09-28 07:29:04 -0500

    Initial Commit

---
+++ KiteProjectOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KiteProjectOne
+//
+//  Created by Zoe Paula Xanos on 9/28/18.
+//  Copyright © 2018 Zoe Paula Xanos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
